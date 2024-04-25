17b618a80dfda92e6f0d85d3c0a6aeed260ee6d3
Harshu29-09 <131910274+Harshu29-09@users.noreply.github.com>
2024-04-25 13:20:21 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,17 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    let longest = 0;
+    let start = 0;
+    let charIndexMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (charIndexMap[s[i]] >= start) {
+            start = charIndexMap[s[i]] + 1;
+        }
+        charIndexMap[s[i]] = i;
+        longest = Math.max(longest, i - start + 1);
+    }
+
+    return longest;
 }
 
 module.exports = { longestSubstring };
