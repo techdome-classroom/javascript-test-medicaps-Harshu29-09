408d020e9aa41260725e199fd42a791aca341c57
Harshu29-09 <131910274+Harshu29-09@users.noreply.github.com>
2024-04-25 13:22:22 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,6 +1,20 @@
 function smallestMissingPositiveInteger(nums) {
 
-  // Implement the function smallest_missing_positive_integer
+  const n = nums.length;
+    
+  for (let i = 0; i < n; i++) {
+      while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+          [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+      }
+  }
+  
+  for (let i = 0; i < n; i++) {
+      if (nums[i] !== i + 1) {
+          return i + 1;
+      }
+  }
+  
+  return n + 1;
 
 }
 
